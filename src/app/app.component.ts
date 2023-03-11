e1d2ffe8ea219eb554ce8f8aa4a172f3c7323702
ANIES AHAMED <aniesahamedm786@gmail.com>
2023-03-11 22:03:57 +0530

    changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-
+import { Router } from '@angular/router';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +7,10 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Media_Monk';
+  constructor(private router: Router){}
+
+
+  ngOnInit(){
+    this.router.navigateByUrl("main");
+  }
 }
